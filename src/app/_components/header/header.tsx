beda497f0dc6b4b7f041e0b1919bbe8951bb376d
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-08-31 16:33:19 -0500

    Feat: Header creation

---
+++ src/app/_components/header/header.tsx
@@ -0,0 +1,32 @@
+export default function Header() {
+    
+    return (
+        <header>
+            <div className="flex justify-around mt-2">
+
+                <div>
+                    <span className="mr-3 text-sm"><i className="fa-solid fa-language"></i> USD <i className="fa-solid fa-chevron-down"></i></span>
+                    <span className="text-sm">English <i className="fa-solid fa-chevron-down"></i></span>
+                </div>
+
+                <h1 className="text-3xl font-bold ">ecomus</h1>
+
+                <div>
+                    <i className="fa-solid fa-magnifying-glass mr-3"></i>
+                    <i className="fa-regular fa-user mr-3"></i>
+                    <i className="fa-regular fa-heart mr-3"></i>
+                    <i className="fa-solid fa-bag-shopping mr-3"></i>
+                </div>
+            </div>
+
+            <div className="flex justify-center mt-6">
+                <p className="mr-6">Home <i className="fa-solid fa-chevron-down"></i></p>
+                <p className="mr-6">Shop <i className="fa-solid fa-chevron-down"></i></p>
+                <p className="mr-6">Products <i className="fa-solid fa-chevron-down"></i></p>
+                <p className="mr-6">Page <i className="fa-solid fa-chevron-down"></i></p>
+                <p className="mr-6">Blog <i className="fa-solid fa-chevron-down"></i></p>
+                <p className="mr-6">Buy now</p>
+            </div>
+        </header>
+    );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/header/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+          />
+      </head>
+      <body className={inter.className}>
+        <Header />
+        {children}</body>
     </html>
   );
 }
