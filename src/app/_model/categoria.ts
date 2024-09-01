a9b70114e082f9f13f05e3aee3512851caf1ca19
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-09-01 02:34:35 -0500

    Feat: Session collection creation

---
+++ src/app/_model/categoria.ts
@@ -0,0 +1,10 @@
+import { Category } from "../_interface/Categoria";
+
+export const categories: Category[] = [
+  { title: "Women´s", items: "23", img: "/img/img1.jpg" },
+  { title: "Men´s", items: "9", img: "/img/img1.jpg" },
+  { title: "Jewelry", items: "31", img: "/img/img1.jpg" },
+  { title: "Sneakers", items: "21", img: "/img/img1.jpg" },
+  { title: "Bags", items: "5", img: "/img/img1.jpg" },
+  { title: "Glasses", items: "14", img: "/img/img1.jpg" },
+];
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import SessionLeft from "../_components/session/sessionLeft";
+import SessionCollection from "../_components/sessionCollection/sessionCollection";
 
 export default function Home() {
   return (
     <>
       <SessionLeft />
+      <SessionCollection />
     </>
   );
 }
