a6994d5626f136bf9362c9b0ad7cd8867aece7ea
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-09-01 11:58:55 -0500

    Feat: Session Shop cretion

---
+++ src/app/_interface/cardShop.ts
@@ -0,0 +1,6 @@
+export interface CardShop {
+
+    title: string,
+    subtitle: string,
+    img: string,
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import Menu from "../_components/menu/menu";
 import SessionLeft from "../_components/session/sessionLeft";
 import SessionCard from "../_components/sessionCard/sessionCard";
 import SessionCollection from "../_components/sessionCollection/sessionCollection";
+import SessionShop from "../_components/sessionShop/sessionShop";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Menu />
       <SessionCard />
       <Button />
+      <SessionShop />
     </>
   );
 }
