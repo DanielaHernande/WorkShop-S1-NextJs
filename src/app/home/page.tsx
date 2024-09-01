b22fab6e39cc314eee9cd108a86b639f0951226d
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-09-01 12:31:24 -0500

    Feat: Payment  cretion

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import Button from "../_components/button/button";
 import Menu from "../_components/menu/menu";
+import Payment from "../_components/payment/payment";
 import SessionLeft from "../_components/session/sessionLeft";
 import SessionCard from "../_components/sessionCard/sessionCard";
 import SessionCollection from "../_components/sessionCollection/sessionCollection";
@@ -14,6 +15,7 @@ export default function Home() {
       <SessionCard />
       <Button />
       <SessionShop />
+      <Payment />
     </>
   );
 }
